2d2da6d3eb6fd38363a8626e28b9f2c924e0e7c8
Joshua Burger <joshberger6969@outlook.com>
2020-12-10 14:24:53 -0500

    Done.

---
+++ reverse_each_word.rb
@@ -0,0 +1,17 @@
+def reverse_each_word(string)
+    array = string.split
+    new_string = ""
+    array.each do |word|
+        new_string += "#{word.reverse} "
+    end
+    return new_string.chop
+end
+
+def reverse_each_word(string)
+    array = string.split
+    new_string = ""
+    array.collect do |word|
+        new_string += "#{word.reverse} "
+    end
+    return new_string.chop
+end
